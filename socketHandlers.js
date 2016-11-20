729b5e32a106263d971b13ddc46e5fe4c9d3986f
Alexander Shipin <alexander.shipin@gmail.com>
2016-11-20 17:37:54 +0300

    backward compatibility with socket.io

--- socketHandlers.js
+++ socketHandlers.js
@@ -4,9 +4,9 @@ module.exports = function (io) {
 	var module = {};
 	module.rooms = [];
 	
-	function getCssValidSocketId(socketId) {
+	/*function getCssValidSocketId(socketId) {
 		return socketId.substring(2);
-	}
+	}*/
 	
 	function getUniqueRoomName(adminSocketId) {
 		//possible collision
@@ -25,12 +25,12 @@ module.exports = function (io) {
 	module.connection = function (socket){
 		socket.on('newPlayer', function (playerName, roomName){
 			var currentPlayer = {
-				id: getCssValidSocketId(socket.id),
+				id: socket.id,
 				name: playerName
 			}
 			var room = module.rooms.filter(r => r.name == roomName);
 			if(roomName === '' || room.length == 0) {
-				roomName = getUniqueRoomName(getCssValidSocketId(socket.id));
+				roomName = getUniqueRoomName(socket.id);
 				var room = {name: roomName, players: []};
 				module.rooms.push(room);
 			}			
@@ -43,11 +43,11 @@ module.exports = function (io) {
 		});
 
 		socket.on('disconnect', function () {
-			io.emit('playerDisconnected', getCssValidSocketId(socket.id));
+			io.emit('playerDisconnected', socket.id);
 			if(!socket.roomName)
 				return;
 			var players = getRoomPlayers(socket.roomName);
-			var disconnectedPlayerIndex = players.findIndex(function(p){ return p.id == getCssValidSocketId(socket.id)});
+			var disconnectedPlayerIndex = players.findIndex(function(p){ return p.id == socket.id});
 			if(disconnectedPlayerIndex >= 0) {
 				players.splice(disconnectedPlayerIndex, 1);
 				socket.leave(socket.roomName);
@@ -69,13 +69,13 @@ module.exports = function (io) {
 				return;
 				
 			var newCommonCard;
-			if(cards.playerHands[getCssValidSocketId(socket.id)].length > 0)
-				newCommonCard = cards.playerHands[getCssValidSocketId(socket.id)].splice(0, 1)[0];
+			if(cards.playerHands[socket.id].length > 0)
+				newCommonCard = cards.playerHands[socket.id].splice(0, 1)[0];
 			else
 				newCommonCard = -1;
 			cards.commonCard = newCommonCard;
 			
-			var newCurrentPlayerCardId = cards.playerHands[getCssValidSocketId(socket.id)][0];
+			var newCurrentPlayerCardId = cards.playerHands[socket.id][0];
 			
 			io.to(socket.roomName).emit('changeCommonCard', cardMng.getCommonCardMarkup(newCommonCard));
 			socket.emit('changePlayerCard', cardMng.getPlayerCardMarkup(newCurrentPlayerCardId));
@@ -88,7 +88,7 @@ module.exports = function (io) {
 			
 			for(var i = 0; i < players.length; i++) {
 				var currentPlayerCardId = room.cards.playerHands[players[i].id][0];
-				io.to("/#" + players[i].id).emit('changePlayerCard', cardMng.getPlayerCardMarkup(currentPlayerCardId));
+				io.to(players[i].id).emit('changePlayerCard', cardMng.getPlayerCardMarkup(currentPlayerCardId));
 			}
 			io.to(socket.roomName).emit('changeCommonCard', cardMng.getCommonCardMarkup(room.cards.commonCard));
 			console.log('start');
