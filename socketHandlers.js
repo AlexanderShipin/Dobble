13333bd61aeca821afe251d38e46961dd9447a67
Alexander Shipin <alexander.shipin@gmail.com>
2016-12-26 23:09:35 +0300

    results table (with time)

--- socketHandlers.js
+++ socketHandlers.js
@@ -4,22 +4,14 @@ module.exports = function (io) {
 	var module = {};
 	module.rooms = [];
 	
-	/*function getCssValidSocketId(socketId) {
-		return socketId.substring(2);
-	}*/
-	
 	function getUniqueRoomName(adminSocketId) {
 		//possible collision
 		var lowerCaseSocketId = adminSocketId.toLowerCase();
 		return lowerCaseSocketId.substring(0, 5);
 	}
 	
-	function getRoomPlayers(roomName) {
-		return module.rooms.filter(r => r.name == roomName)[0].players;
-	}
-	
-	function getRoomCards(roomName) {
-		return module.rooms.filter(r => r.name == roomName)[0].cards;
+	function getRoom(roomName) {
+		return module.rooms.find(r => r.name == roomName);
 	}
 	
 	module.connection = function (socket){
@@ -28,8 +20,8 @@ module.exports = function (io) {
 				id: socket.id,
 				name: playerName
 			}
-			var room = module.rooms.filter(r => r.name == roomName);
-			if(roomName === '' || room.length == 0) {
+			var room = module.rooms.find(r => r.name == roomName);
+			if(roomName === '' || !room) {
 				roomName = getUniqueRoomName(socket.id);
 				var room = {name: roomName, players: []};
 				module.rooms.push(room);
@@ -37,8 +29,8 @@ module.exports = function (io) {
 			socket.roomName = roomName;
 			socket.join(roomName);
 			
-			getRoomPlayers(socket.roomName).push(currentPlayer);
-			io.to(socket.roomName).emit('roomPlayers', JSON.stringify(getRoomPlayers(socket.roomName)), socket.roomName);
+			getRoom(socket.roomName).players.push(currentPlayer);
+			io.to(socket.roomName).emit('roomPlayers', JSON.stringify(getRoom(socket.roomName).players), socket.roomName);
 			console.log('user ' + socket.id + ' connected');
 		});
 
@@ -46,7 +38,7 @@ module.exports = function (io) {
 			io.emit('playerDisconnected', socket.id);
 			if(!socket.roomName)
 				return;
-			var players = getRoomPlayers(socket.roomName);
+			var players = getRoom(socket.roomName).players;
 			var disconnectedPlayerIndex = players.findIndex(function(p){ return p.id == socket.id});
 			if(disconnectedPlayerIndex >= 0) {
 				players.splice(disconnectedPlayerIndex, 1);
@@ -60,7 +52,8 @@ module.exports = function (io) {
 		});
 
 		socket.on('selectPict', function (selectedPicName) {
-			var cards = getRoomCards(socket.roomName);
+			var room = getRoom(socket.roomName);
+			var cards = room.cards;
 			if(!cards)
 				return;
 				
@@ -79,18 +72,28 @@ module.exports = function (io) {
 			
 			io.to(socket.roomName).emit('changeCommonCard', cardMng.getCommonCardMarkup(newCommonCard));
 			socket.emit('changePlayerCard', cardMng.getPlayerCardMarkup(newCurrentPlayerCardId));
+			
+			if(cards.playerHands[socket.id].length == 0) {
+				var finishedPlayer = room.players.find(p => p.id == socket.id);
+				var time = process.hrtime(room.startTime);
+				room.finishedPlayers.push({id: socket.id, name: finishedPlayer.name, time: time[0] + Math.round(time[1]/1000000000 * 100) / 100 + "s" });
+				room.finishedPlayers.forEach(fp => io.to(fp.id).emit('playerFinished', room.finishedPlayers));
+			}		
 		});
 
 		socket.on('start', function () {
-			var players = getRoomPlayers(socket.roomName);
-			var room = module.rooms.filter(r => r.name == socket.roomName)[0];
+			var room = getRoom(socket.roomName);
+			var players = room.players;	
 			room.cards = cardMng.prepareCards(players);
 			
 			for(var i = 0; i < players.length; i++) {
 				var currentPlayerCardId = room.cards.playerHands[players[i].id][0];
 				io.to(players[i].id).emit('changePlayerCard', cardMng.getPlayerCardMarkup(currentPlayerCardId));
 			}
+			io.to(socket.roomName).emit('start');
 			io.to(socket.roomName).emit('changeCommonCard', cardMng.getCommonCardMarkup(room.cards.commonCard));
+			room.finishedPlayers = [];
+			room.startTime = process.hrtime();
 			console.log('start');
 		});
 	};
